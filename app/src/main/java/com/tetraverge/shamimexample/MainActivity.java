f7c7ed07f462064a0aec64bf35cfda37e6b58424
rabbii <rabbiulipur@gmail.com>
2021-08-26 19:53:44 +0600

    First

--- app/src/main/java/com/tetraverge/shamimexample/MainActivity.java
+++ app/src/main/java/com/tetraverge/shamimexample/MainActivity.java
@@ -3,12 +3,45 @@ package com.tetraverge.shamimexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    EditText texta,textb,textc;
+    Button calcBtn;
+    TextView calView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        texta = (EditText) findViewById(R.id.atext);
+        textb = (EditText) findViewById(R.id.btext);
+        textc = (EditText) findViewById(R.id.ctext);
+        calcBtn = (Button) findViewById(R.id.btn);
+        calView = (TextView) findViewById(R.id.calculatext);
+
+        calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String a = String.valueOf(texta.getText());
+                String b = String.valueOf(textb.getText());
+                String c = String.valueOf(textc.getText());
+                int cona = Integer.parseInt(a);
+                int conb = Integer.parseInt(b);
+                int conc = Integer.parseInt(c);
+
+                int result = cona+conb+conc;
+                calView.setText(Integer.toString(result));
+
+            }
+        });
+
     }
 }
